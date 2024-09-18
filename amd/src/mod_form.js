15e6dfe06c2db3cf6bef718157cb47ce04a42126
Joe Conradt <joeconradt@gmail.com>
2024-09-17 22:03:40 -0500

    js lint fixes

--- amd/src/mod_form.js
+++ amd/src/mod_form.js
@@ -3,6 +3,7 @@ import VideoManagerModal from 'mod_video/video_manager_modal';
 import ModalFactory from 'core/modal_factory';
 import ModalEvents from 'core/modal_events';
 import $ from 'jquery';
+import Notification from 'core/notification';
 
 /**
  * Initialize functionality for module create/edit form.
@@ -16,27 +17,29 @@ export function init(uniqueId, videoSourceType, inputId, debug) {
 
     ModalFactory.create({
         type: VideoManagerModal.TYPE,
-        templateContext: { uniqueid: uniqueId },
+        templateContext: {uniqueid: uniqueId},
         large: true,
-    }, $(`#id_searchvideos_${videoSourceType}`)).then(function (modal) {
-        modal.getRoot().on(ModalEvents.shown, function () {
-            if (!videoManagerInstance) {
-                videoManagerInstance = new VideoManager(
-                    uniqueId,
-                    videoSourceType,
-                    {
-                        onVideoSelect: (selectedVideo) => {
-                            document.getElementById(inputId).value = selectedVideo.videoid;
-                            modal.hide();
-                        },
-                        debug,
-                    }
-                );
-            }
-        });
-
-        modal.getRoot().on('click', '.close-button', () => {
-            modal.hide();
-        });
-    });
+    }, $(`#id_searchvideos_${videoSourceType}`))
+        .then(function(modal) {
+            modal.getRoot().on(ModalEvents.shown, function() {
+                if (!videoManagerInstance) {
+                    videoManagerInstance = new VideoManager(
+                        uniqueId,
+                        videoSourceType,
+                        {
+                            onVideoSelect: (selectedVideo) => {
+                                document.getElementById(inputId).value = selectedVideo.videoid;
+                                modal.hide();
+                            },
+                            debug,
+                        }
+                    );
+                }
+            });
+            modal.getRoot().on('click', '.close-button', () => {
+                modal.hide();
+            });
+            return modal;
+        })
+        .catch(Notification.exception);
 }
--- amd/src/video_manager.js
+++ amd/src/video_manager.js
@@ -27,12 +27,12 @@ export default class VideoManager {
         this.log("video_manager:init:uniqueid", this.uniqueId);
         this.log("video_manager:init:options", this.options);
 
-        this.searchButton.addEventListener('click', async () => {
+        this.searchButton.addEventListener('click', async() => {
             await this.query();
             await this.refreshResults();
         });
 
-        this.searchInput.addEventListener('keypress', async (e) => {
+        this.searchInput.addEventListener('keypress', async(e) => {
             if (e.key === 'Enter') {
                 e.preventDefault();
                 await this.query();
@@ -49,14 +49,14 @@ export default class VideoManager {
         this.setLoading(true);
         try {
             this.videoResults = (await this.queryVideos(this.searchInput.value)).results;
-        } catch(e) {
+        } catch (e) {
             await Notification.exception(e);
         }
     }
 
     async refreshResults() {
-        const renderedResults = await Promise.all(this.videoResults.videos.map(async (videoResult) => {
-            const { html } = await Templates.renderForPromise('mod_video/video_manager_result', videoResult);
+        const renderedResults = await Promise.all(this.videoResults.videos.map(async(videoResult) => {
+            const {html} = await Templates.renderForPromise('mod_video/video_manager_result', videoResult);
             return html;
         }));
 
