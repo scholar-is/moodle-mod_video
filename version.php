c4fed4c3993fe7ac31c8b0cde127452936c605a1
Joe Conradt <joeconradt@gmail.com>
2024-09-19 17:47:22 -0500

    update version

--- version.php
+++ version.php
@@ -25,8 +25,9 @@
 defined('MOODLE_INTERNAL') || die;
 
 $plugin->component = 'mod_video'; // Full name of the plugin (used for diagnostics).
-$plugin->version   = 2024091801;  // The current module version (Date: YYYYMMDDXX).
+$plugin->version   = 2024091900;  // The current module version (Date: YYYYMMDDXX).
 $plugin->requires  = 2022030300;  // Requires this Moodle version.
 $plugin->cron      = 0;           // Period for cron to check this module (secs).
 $plugin->release   = '0.1';
 $plugin->maturity  = MATURITY_BETA;
+$plugin->supported = [400, 404];
