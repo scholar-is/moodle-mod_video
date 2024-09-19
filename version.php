3019a0a882d9287217327765f011002da7b133fc
Joe Conradt <joeconradt@gmail.com>
2024-09-19 14:52:10 -0500

    fix: update plugin requires version

--- version.php
+++ version.php
@@ -26,7 +26,7 @@ defined('MOODLE_INTERNAL') || die;
 
 $plugin->component = 'mod_video'; // Full name of the plugin (used for diagnostics).
 $plugin->version   = 2024091801;  // The current module version (Date: YYYYMMDDXX).
-$plugin->requires  = 2019111809;  // Requires this Moodle version.
+$plugin->requires  = 2022030300;  // Requires this Moodle version.
 $plugin->cron      = 0;           // Period for cron to check this module (secs).
 $plugin->release   = '0.1';
 $plugin->maturity  = MATURITY_BETA;
